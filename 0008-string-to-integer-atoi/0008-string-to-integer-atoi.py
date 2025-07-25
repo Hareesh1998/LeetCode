784f8d5bc7d96c5f1224c4cbc44a738dec997b44
Hareesh1998 <48708652+Hareesh1998@users.noreply.github.com>
2025-07-25 16:14:44 -0500

    Time: 0 ms (100%), Space: 17.8 MB (77.51%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,42 +1,35 @@
 class Solution:
     def myAtoi(self, str: str) -> int:
-        # Return 0 if string is empty
+       
         if not str:
             return 0
 
         length_of_string = len(str)
         index = 0
 
-        # Skip leading whitespaces
+     
         while index < length_of_string and str[index] == ' ':
             index += 1
 
-        # After skipping whitespaces, if we're at the end it means it's an empty string
         if index == length_of_string:
             return 0
 
-        # Check if we have a sign, and set the sign accordingly
         sign = -1 if str[index] == '-' else 1
-        if str[index] in ['-', '+']:  # skip the sign for next calculations
+        if str[index] in ['-', '+']:  
             index += 1
 
         result = 0
-        max_safe_int = (2 ** 31 - 1) // 10  # Precomputed to avoid overflow
+        max_safe_int = (2 ** 31 - 1) // 10  
 
         while index < length_of_string:
-            # If the current character is not a digit, break from loop
             if not str[index].isdigit():
                 break
 
             digit = int(str[index])
 
-            # Check for overflow cases
             if result > max_safe_int or (result == max_safe_int and digit > 7):
-                return 2 ** 31 - 1 if sign == 1 else -2 ** 31  # Clamp to INT_MIN or INT_MAX
-
-            # Append current digit to result
+                return 2 ** 31 - 1 if sign == 1 else -2 ** 31  
             result = result * 10 + digit
             index += 1
 
-        # Apply sign and return final result
         return sign * result
